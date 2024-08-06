0d45437680e24aea55e050a018dfd6b20d556936
Muhammad zulfatah <zfath423@gmail.com>
2024-08-06 08:29:05 +0700

    update server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const mysql = require('mysql2/promise');
 
 const init = async () => {
 
@@ -9,11 +10,30 @@ const init = async () => {
         host: '0.0.0.0'
     });
 
+    // Create a connection pool to the MySQL database
+    const pool = mysql.createPool({
+        host: '180.254.158.159',
+        user: 'newuser2',
+        password: 'Zulfath423#',
+        database: 'data_pasien'
+    });
+
+    // Sample route to fetch data from MySQL
     server.route({
         method: 'GET',
         path: '/',
-        handler: (request, h) => {
-            return 'Hello, World!';
+        handler: async (request, h) => {
+            let connection;
+            try {
+                connection = await pool.getConnection();
+                const [rows, fields] = await connection.execute('SELECT * data_antrian');
+                return rows;
+            } catch (err) {
+                console.error(err);
+                return h.response('Internal Server Error').code(500);
+            } finally {
+                if (connection) connection.release();
+            }
         }
     });
 
